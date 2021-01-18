b2125fd6735500b826755982aa987dc1d5a683df
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-01-18 21:59:21 +0000

    Cross origins for feature 1

--- src/main/java/com/mohamed/hadramy/firstonlinebookstorespringboot/repository/BookRepository.java
+++ src/main/java/com/mohamed/hadramy/firstonlinebookstorespringboot/repository/BookRepository.java
@@ -2,10 +2,11 @@ package com.mohamed.hadramy.firstonlinebookstorespringboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.mohamed.hadramy.firstonlinebookstorespringboot.model.Book;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
