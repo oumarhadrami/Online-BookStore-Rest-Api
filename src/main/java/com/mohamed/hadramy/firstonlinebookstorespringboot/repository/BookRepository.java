3646c490cc931ca8655bf5a16e8e9057df0e58e7
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-01-19 21:45:24 +0000

    Add Search by Query to master

--- src/main/java/com/mohamed/hadramy/firstonlinebookstorespringboot/repository/BookRepository.java
+++ src/main/java/com/mohamed/hadramy/firstonlinebookstorespringboot/repository/BookRepository.java
@@ -1,12 +1,18 @@
 package com.mohamed.hadramy.firstonlinebookstorespringboot.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.mohamed.hadramy.firstonlinebookstorespringboot.model.Book;
 
 @CrossOrigin(origins = "http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+	
+	@RestResource(path = "category_id")
+	Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
